02fd1da76195682c42c47da7b510839be49a298e
Fernanda Kipper <nanda.kipper@gmail.com>
2022-10-19 08:06:02 -0300

    fix: adjust c example to return pointer to array greather then 1

--- calculate.c
+++ calculate.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 int *calculate(int i){
-    int *result = (int *)malloc(sizeof(int) * 2);
+    int *result = (int *)malloc(sizeof(int) * 3);
     // return size of the array on the first position
     result[0] = 2;
     result[1] = i;
+    result[2] = i + 4;
 
     return result;
 }
